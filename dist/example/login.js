0fba8aa84a378f2463928def1625fe68d0b17476
Jacky Tang <tangyingzong@gmail.com>
2017-08-02 22:39:41 +0800

    add login

---
+++ dist/example/login.js
@@ -0,0 +1,28 @@
+﻿//$(function () {
+//    login(
+//        var btn1 = document.getElementById('login');
+//    alert(btn1.innerHTML);
+
+//        )
+    
+
+//});
+
+
+$("#login").click(function (event) {
+    /* Act on the event */
+    $.ajax({
+        "url": sysSettings.domainPath + "BVSP_CUSTOMER_SEARCH",
+        "async": true,
+        "crossDomain": true,
+        "type": "POST",
+        "dataType": "json",
+        "contentType": "application/json; charset=utf-8",
+        "data": function (data) {
+            var param = {
+                "token": SecurityManager.generate(),
+            }
+            alert(data);
+        }
+    })
+});
