4aca38697d337f70a63b31cf1ca670046cbbaff8
Jacky Tang <tangyingzong@gmail.com>
2017-08-03 18:11:03 +0800

    添加激活页面

---
+++ dist/example/genvoucher.js
@@ -0,0 +1,34 @@
+﻿
+$("#genvoucher").click(function (event) {
+
+    alert(123123);
+    var param = {
+        "Phone": $("#telno").text(),
+        "PIN_Code": $("#pincode").val()
+    }
+    $.ajax({
+        "url": "https://www.billionvouchers.com/BVwebapi/" + "BVSP_VOUCHER_GENERATE",
+        "async": true,
+        "crossDomain": true,
+        "type": "POST",
+        "dataType": "json",
+        "contentType": "application/json; charset=utf-8",
+        "data": JSON.stringify(param),
+        "success": function (result) {
+
+            if (result.ResultSets.length == 0) {
+                console.log('激活成功');
+               
+                location.href = "msg_success_genvoucher.html";
+            }
+            else if (result.ResultSets[0].length > 0) {
+                console.log('激活失败：' + result.ResultSets[0][0].msg);
+                location.href = "msg_warn_genvoucher.html";
+                
+            }
+            else {
+
+            }
+        }
+    })
+});
--- dist/example/home.js
+++ dist/example/home.js
@@ -1,1 +1,14 @@
-﻿
+﻿//$(function () {
+//    login(
+//        var btn1 = document.getElementById('login');
+//    alert(btn1.innerHTML);
+
+//        )
+
+
+//});
+
+
+$("#genvoucher").click(function (event) {
+    location.href = 'genvoucher.html';
+});
